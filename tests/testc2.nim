479e181fa318ef8ada8559eeb900aaac7597fadf
Ethosa <social.ethosa@gmail.com>
2023-05-12 17:04:15 +0700

    add optional with default path params

--- tests/testc2.nim
+++ tests/testc2.nim
@@ -19,7 +19,35 @@ serve("127.0.0.1", 5000):
       "true!"
     else:
       "false!"
-  
+
+  get "/optional/{arg?:bool}{arg1?:int}{arg2?:bool}{arg3?:float}{arg4?:string}":
+    req.answerHtml:
+      buildHtml:
+        tDiv:
+          "arg is {arg}"
+        tDiv:
+          "arg1 is {arg1}"
+        tDiv:
+          "arg2 is {arg2}"
+        tDiv:
+          "arg3 is {arg3}"
+        tDiv:
+          "arg4 is \"{arg4}\""
+
+  get "/default/{arg?:bool=true}{arg1:bool=true}{arg2?:int=123}{arg3:int=123123}{arg4:word=Hi}":
+    req.answerHtml:
+      buildHtml:
+        tDiv:
+          "arg is {arg}"
+        tDiv:
+          "arg1 is {arg1}"
+        tDiv:
+          "arg2 is {arg2}"
+        tDiv:
+          "arg3 is {arg3}"
+        tDiv:
+          "arg4 is \"{arg4}\""
+
   wsConnect:
     echo "New connection!"
     await wsClient.send("You're welcome!")
