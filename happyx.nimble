2f9a322fbf77218301cd0cb9fb7808ddc2cc8e22
Ethosa <social.ethosa@gmail.com>
2023-05-12 17:04:26 +0700

    add optional with default path params

--- happyx.nimble
+++ happyx.nimble
@@ -2,7 +2,7 @@
 
 description = "Asynchronous web-framework written with ♥"
 author = "HapticX"
-version = "0.12.0"
+version = "0.13.0"
 license = "GNU GPLv3"
 srcDir = "src"
 installExt = @["nim"]
--- src/happyx/private/cmpltime.nim
+++ src/happyx/private/cmpltime.nim
@@ -1,4 +1,5 @@
 import
+  strutils,
   macros,
   regex
 
@@ -12,30 +13,53 @@ proc exportRouteArgs*(urlPath, routePath, body: NimNode): NimNode {.compileTime.
     routePathStr = $routePath
     hasChildren = false
   # boolean param
-  routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*:bool\}", "(n|y|no|yes|true|false|1|0|on|off)")
+  routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*(\??):bool(=\S+?)?\}", "(n|y|no|yes|true|false|1|0|on|off)$1")
   # integer param
-  routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*:int\}", "(\\d+)")
+  routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*(\??):int(=\S+?)?\}", "(\\d+)$1")
   # float param
-  routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*:float\}", "(\\d+\\.\\d+|\\d+)")
+  routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*(\??):float(=\S+?)?\}", "(\\d+\\.\\d+|\\d+)$1")
   # word param
-  routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*:word\}", "(\\w+?)")
+  routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*(\??):word(=\S+?)?\}", "(\\w+?)$1")
   # string param
-  routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*:string\}", "([^/]+?)")
+  routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*(\??):string(=\S+?)?\}", "([^/]+?)$1")
   # path param
   routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*:path\}", "([\\S]+)")
   # path param
   routePathStr = routePathStr.replace(re"\{[a-zA-Z][a-zA-Z0-9_]*:/([\s\S]+?)/\}", "($1)")
+  # repair param
+  routePathStr = routePathStr.replace(re"(\([^\(\)]+?\))(?!\?)", "$1?")
   let
     regExp = newCall("re", newStrLitNode("^" & routePathStr & "$"))
-    found = path.findAll(re"\{([a-zA-Z][a-zA-Z0-9_]*):(bool|int|float|string|path|word|/[\s\S]+?/)\}")
+    found = path.findAll(
+      re"\{([a-zA-Z][a-zA-Z0-9_]*\??):(bool|int|float|string|path|word|/[\s\S]+?/)(=(\S+?))?\}"
+    )
+  echo treeRepr regExp
   elifBranch.add(newCall("contains", urlPath, regExp), body)
-  var idx = 0
+  var
+    idx = 0
+    name = ""
+    isOptional = false
+    defaultVal = ""
   for i in found:
+    # clean
+    name = i.group(0, path)[0]
+    isOptional = false
+    defaultVal =
+      if i.groupsCount < 4 or i.group(3, path).len == 0:
+        ""
+      else:
+        echo i.group(3, path)[0]
+        i.group(3, path)[0]
+    # detect optional
+    if name.endsWith(re"\?"):
+      name = name[0..^2]
+      isOptional = true
+    elif defaultVal.len > 0:
+      isOptional = true
     let
-      name = ident(i.group(0, path)[0])
       argTypeStr = i.group(1, path)[0]
       letSection = newNimNode(nnkLetSection).add(
-        newNimNode(nnkIdentDefs).add(name, newEmptyNode())
+        newNimNode(nnkIdentDefs).add(ident(name), newEmptyNode())
       )
       foundGroup = newNimNode(nnkBracketExpr).add(
         newCall(
@@ -46,18 +70,128 @@ proc exportRouteArgs*(urlPath, routePath, body: NimNode): NimNode {.compileTime.
         ),
         newIntLitNode(0)
       )
-    case argTypeStr:
-    of "bool":
-      letSection[0].add(newCall("parseBool", foundGroup))
-    of "int":
-      letSection[0].add(newCall("parseInt", foundGroup))
-    of "float":
-      letSection[0].add(newCall("parseFloat", foundGroup))
-    of "path", "string", "word":
-      letSection[0].add(foundGroup)
+    if isOptional:
+      case argTypeStr:
+      of "bool":
+        letSection[0].add(newNimNode(nnkIfStmt).add(
+            newNimNode(nnkElifBranch).add(
+              newCall(
+                "<",
+                newCall("len", newCall(
+                  "group",
+                  newNimNode(nnkBracketExpr).add(ident("founded_regexp_matches"), newIntLitNode(0)),
+                  newIntLitNode(idx),  # group index,
+                  urlPath
+                )),
+                newIntLitNode(1)
+              ),
+              newLit(
+                if defaultVal == "": false else: parseBool(defaultVal)
+              )
+            ),
+            newNimNode(nnkElifBranch).add(
+              newCall("==", newCall("len", foundGroup), newIntLitNode(0)),
+              newLit(
+                if defaultVal == "": false else: parseBool(defaultVal)
+              )
+            ),
+            newNimNode(nnkElse).add(newCall("parseBool", foundGroup))
+          )
+        )
+      of "int":
+        letSection[0].add(newNimNode(nnkIfStmt).add(
+            newNimNode(nnkElifBranch).add(
+              newCall(
+                "<",
+                newCall("len", newCall(
+                  "group",
+                  newNimNode(nnkBracketExpr).add(ident("founded_regexp_matches"), newIntLitNode(0)),
+                  newIntLitNode(idx),  # group index,
+                  urlPath
+                )),
+                newIntLitNode(1)
+              ),
+              newLit(
+                if defaultVal == "": 0 else: parseInt(defaultVal)
+              )
+            ),
+            newNimNode(nnkElifBranch).add(
+              newCall("==", newCall("len", foundGroup), newIntLitNode(0)),
+              newLit(
+                if defaultVal == "": 0 else: parseInt(defaultVal)
+              )
+            ),
+            newNimNode(nnkElse).add(newCall("parseInt", foundGroup))
+          )
+        )
+      of "float":
+        letSection[0].add(newNimNode(nnkIfStmt).add(
+            newNimNode(nnkElifBranch).add(
+              newCall(
+                "<",
+                newCall("len", newCall(
+                  "group",
+                  newNimNode(nnkBracketExpr).add(ident("founded_regexp_matches"), newIntLitNode(0)),
+                  newIntLitNode(idx),  # group index,
+                  urlPath
+                )),
+                newIntLitNode(1)
+              ),
+              newLit(
+                if defaultVal == "": 0.0 else: parseFloat(defaultVal)
+              )
+            ),
+            newNimNode(nnkElifBranch).add(
+              newCall("==", newCall("len", foundGroup), newIntLitNode(0)),
+              newLit(
+                if defaultVal == "": 0.0 else: parseFloat(defaultVal)
+              )
+            ),
+            newNimNode(nnkElse).add(newCall("parseFloat", foundGroup))
+          )
+        )
+      of "string", "word":
+        letSection[0].add(newNimNode(nnkIfStmt).add(
+            newNimNode(nnkElifBranch).add(
+              newCall(
+                "<",
+                newCall("len", newCall(
+                  "group",
+                  newNimNode(nnkBracketExpr).add(ident("founded_regexp_matches"), newIntLitNode(0)),
+                  newIntLitNode(idx),  # group index,
+                  urlPath
+                )),
+                newIntLitNode(1)
+              ),
+              newLit(
+                if defaultVal == "": "" else: defaultVal
+              )
+            ),
+            newNimNode(nnkElifBranch).add(
+              newCall("==", newCall("len", foundGroup), newIntLitNode(0)),
+              newLit(
+                if defaultVal == "": "" else: defaultVal
+              )
+            ),
+            newNimNode(nnkElse).add(foundGroup)
+          )
+        )
+      else:
+        # regex
+        letSection[0].add(foundGroup)
     else:
-      # regex
-      letSection[0].add(foundGroup)
+      case argTypeStr:
+      of "bool":
+        letSection[0].add(newCall("parseBool", foundGroup))
+      of "int":
+        letSection[0].add(newCall("parseInt", foundGroup))
+      of "float":
+        letSection[0].add(newCall("parseFloat", foundGroup))
+      of "path", "string", "word":
+        letSection[0].add(foundGroup)
+      else:
+        # regex
+        letSection[0].add(foundGroup)
     elifBranch[1].insert(0, letSection)
     hasChildren = true
     inc idx
