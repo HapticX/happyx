2da37b008e99ff5b598566739d67271d6439350e
Ethosa <social.ethosa@gmail.com>
2023-07-07 11:42:35 +1000

    little improve

--- examples/website/src/components/card.nim
+++ examples/website/src/components/card.nim
@@ -9,6 +9,6 @@ component Card:
   `template`:
     tDiv(
       id = "{self.id}",
-      class = "flex will-change-transform justify-center items-center gap-12 w-fit drop-shadow-2xl rounded-md bg-[{Background}] dark:bg-[{BackgroundDark}] text-3xl md:text-2xl lg:text-xl xl:text-base pr-4"
+      class = "flex will-change-transform justify-center items-center gap-12 w-fit drop-shadow-2xl rounded-md bg-gradient-to-r bg-[{Background}] dark:from-[{BackgroundDark}] dark:to-[{BackgroundSecondaryDark}] text-3xl md:text-2xl lg:text-xl xl:text-base pr-4"
     ):
       slot
