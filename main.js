dd8eec3128abc55ea5d08697ecd573ebf4bd7add
Ethosa <Ethosa@users.noreply.github.com>
2023-08-09 09:48:15 +0000

    deploy: 209be8e8e595b16b45002e7d0015fb59e50aa89b

--- main.js
+++ main.js
@@ -8186,17 +8186,17 @@ function find_905969713(a_905969716, item_905969717) {
     result_905969718 = 0;
     Label1: do {
       var i_905969722 = [];
-      var i_1006633000 = 0;
+      var i_1006633003 = 0;
       Label2: do {
           Label3: while (true) {
-          if (!(i_1006633000 < (a_905969716).length)) break Label3;
-            i_905969722 = a_905969716[i_1006633000];
+          if (!(i_1006633003 < (a_905969716).length)) break Label3;
+            i_905969722 = a_905969716[i_1006633003];
             if (eqStrings(i_905969722, item_905969717)) {
             break BeforeRet;
             }
             
             result_905969718 += 1;
-            i_1006633000 += 1;
+            i_1006633003 += 1;
           }
       } while (false);
     } while (false);
