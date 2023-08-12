f3343958aff392d3d310fdde30bccc839b03e295
Ethosa <social.ethosa@gmail.com>
2023-08-12 14:57:19 +0700

    update roadmap

--- examples/website/src/pages/roadmap.nim
+++ examples/website/src/pages/roadmap.nim
@@ -65,3 +65,17 @@ mount RoadMap:
                 "Nim v2.0 Support ⚡"
               tP(class = "w-full text-2xl lg:text-lg xl:text-base"):
                 "Automatic Docs Generation 📕"
+          # v2.1.0
+          component Card():
+            tDiv(class = "w-full flex flex-col items-center gap-2 p-4 lg:p-2 xl:p-0"):
+              tP(class = "text-4xl lg:text-2xl xl:text-lg font-semibold"):
+                "v2.1.0"
+              tP(class = "w-full text-2xl lg:text-lg xl:text-base"):
+                ".hpx files support ✨"
+          # Future
+          component Card():
+            tDiv(class = "w-full flex flex-col items-center gap-2 p-4 lg:p-2 xl:p-0"):
+              tP(class = "text-4xl lg:text-2xl xl:text-lg font-semibold"):
+                "Future releases"
+              tP(class = "w-full text-2xl lg:text-lg xl:text-base"):
+                "Language bindings 🐍"
