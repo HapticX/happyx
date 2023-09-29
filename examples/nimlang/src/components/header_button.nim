62d08a66eb03f799bdcf4547cd15200206576b6a
Ethosa <social.ethosa@gmail.com>
2023-09-29 11:54:28 +0700

    fix examples

--- examples/nimlang/src/components/header_button.nim
+++ examples/nimlang/src/components/header_button.nim
@@ -9,11 +9,12 @@ component HeaderButton:
 
   `template`:
     tButton:
-      class =
+      class := (
         if currentRoute == self.path:
           "flex justify-center items-center text-white h-full px-4 bg-white/10 hover:bg-white/20 active:bg-white/30 transition-colors duration-500"
         else:
           "flex justify-center items-center text-white h-full px-4 hover:bg-white/10 active:bg-white/20 transition-colors duration-500"
+      )
       {self.text}
       @click:
         route(self.path)
