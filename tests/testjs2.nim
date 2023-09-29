750cbfd759c3ecbf89b2388b1af070cc2831fb78
Ethosa <social.ethosa@gmail.com>
2023-09-29 14:27:06 +0700

    fix tests

--- tests/testjs2.nim
+++ tests/testjs2.nim
@@ -72,12 +72,12 @@ app.routes:
   
   "/asd":
     tDiv(a = "1"):
-      b = "2"
+      b := "2"
       @click:
         echo 3
     tDiv:
-      a = "1"
-      b = "2"
+      a := "1"
+      b := "2"
       @click:
         echo 3
 
