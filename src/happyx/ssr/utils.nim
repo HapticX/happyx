93bce85e51aeadece397a4a32038e4be54dcfd27
Ethosa <social.ethosa@gmail.com>
2023-11-03 11:35:22 +0700

    solve #173

--- src/happyx/ssr/utils.nim
+++ src/happyx/ssr/utils.nim
@@ -2,14 +2,19 @@
 ## 
 import
   json,
-  httpcore
+  httpcore,
+  options
 
 
 
-proc toJsonNode*(headers: HttpHeaders): JsonNode =
+proc toJsonNode*(headers: HttpHeaders | Option[HttpHeaders]): JsonNode =
   result = newJObject()
-  for k, v in headers.pairs():
-    result[k] = newJString(v)
+  when headers is HttpHeaders:
+    for k, v in headers.pairs():
+      result[k] = newJString(v)
+  else:
+    for k, v in headers.get().pairs():
+      result[k] = newJString(v)
 
 
 proc toHttpHeaders*(json: JsonNode): HttpHeaders =
