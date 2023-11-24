958dde7263553cc3c7887a181bb716a0f32e697a
Ethosa <social.ethosa@gmail.com>
2023-11-24 12:42:46 +0700

    solve #187

--- tests/testjs15.nim
+++ tests/testjs15.nim
@@ -11,14 +11,54 @@ component withPresets:
 component withSlot:
   s: string = $1
   `template`:
-   "The number is {self.s}, and the slot is "
-   slot
+    "The number is {self.s}, and the slot is "
+    slot
+
+
+
+importComponent "example.hpx" as Example
+importComponent "button.hpx" as ButtonExample
 
 
 appRoutes("app"):
   "/issue183":
     tDiv: component withPresets
     tDiv: component withPresets(s= "I better not be 5")
+
+    if 1 == 2:
+      "Hello!"
+    elif 1 == 3:
+      "Bye!"
+    else:
+      "Oops!"
+    
+    nim:
+      let age = 20
+    
+    tDiv:
+      "Example component 5\n    !!!!\n"
+      if age < 20:
+        tDiv:
+          "You\'re young!\n"
+      elif age < 35:
+        tDiv:
+          "You\'re already young!\n"
+      else:
+        tDiv:
+          "You\'re old!\n"
+      for i in [1, 2, 3, 4]:
+        tDiv:
+          "{i}\n"
+      nim:
+        var x = 10
+      "{x}\n"
+      while x > 0:
+        tDiv("style" = "display: flex; gap: .2rem;"):
+          "{x}\n"
+          nim:
+            dec x
+      tDiv:
+        "123213213213\n"
   
   "/issue184":
     var x = buildHtml(tDiv):
@@ -26,8 +66,8 @@ appRoutes("app"):
     buildHtml:
       x
       x
-      # x:
-      #   "world!"
+      x:
+        "world!"
   
   "/issue185":
     tDiv:
