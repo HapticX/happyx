1ebfe0021ed215cc1509a716482da1c50d91af71
Ethosa <social.ethosa@gmail.com>
2024-05-02 19:51:29 +0700

    fix decorators.nim

--- src/happyx/routing/decorators.nim
+++ src/happyx/routing/decorators.nim
@@ -72,25 +72,25 @@ macro decorator*(name, body: untyped): untyped =
   )
 
 
-when not enableDefaultDecorators:
+when enableDefaultDecorators:
   proc authBasicDecoratorImpl(httpMethods: seq[string], routePath: string, statementList: NimNode, arguments: seq[NimNode]) =
     statementList.insert(0, parseStmt"""
-  var (username, password) = ("", "")
-  if not headers.hasKey("Authorization"):
-    var statusCode = 401
-    return {"response": "failure", "reason": "You should to use Basic authorization!"}
-  else:
-    (username, password) = block:
-      let code = headers["Authorization"].split(" ")[1]
-      let decoded = base64.decode(code).split(":", 1)
-      (decoded[0], decoded[1])"""
+var (username, password) = ("", "")
+if not headers.hasKey("Authorization"):
+  var statusCode = 401
+  return {"response": "failure", "reason": "You should to use Basic authorization!"}
+else:
+  (username, password) = block:
+    let code = headers["Authorization"].split(" ")[1]
+    let decoded = base64.decode(code).split(":", 1)
+    (decoded[0], decoded[1])"""
     )
 
 
   proc getUserAgentDecoratorImpl(httpMethods: seq[string], routePath: string, statementList: NimNode, arguments: seq[NimNode]) =
     statementList.insert(0, parseStmt"""
-  var userAgent = navigator.userAgent
-  """
+var userAgent = navigator.userAgent
+"""
     )
 
 
