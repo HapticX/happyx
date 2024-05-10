29074e144a7fc2e1978015f6180cfcd0787f7330
Ethosa <social.ethosa@gmail.com>
2024-05-10 12:44:20 +0700

    fix onException problem #254

--- tests/testc20.nim
+++ tests/testc20.nim
@@ -1,51 +1,23 @@
 import ../src/happyx
 
+type
+  Kind = enum
+    circle
+    square
 
-type Kind = enum
-  str
-  num
+model Obj:
+  k: Kind
+  st: string
 
+serve("127.0.0.1", 5000):
+  post "/[data:Obj:json]":
+    echo data
+    return ""
 
-model Task:
-  id: int
-
-
-serve "127.0.0.1", 5000:
-  post "/[task:Task]":
-    echo task.id
-    return task.id
-  
-  get "/x/$x":
-    if x == "1":
-      return x
-    echo 1
-    if x == "2":
-      return x
-    echo 2
-    return x
-    echo x
-
-  get "/thisShouldWorkWhithoutRegex":
-    discard
-
-  get "/{k:enum(Kind)}/{x}":
-    "1" & x
-  get "/bool":
-    "2"
-  notfound:
-    "3"
-  
-  staticDir "/testdir"
-
-  staticDir "/testdir" -> "testdir" ~ "html,js,css"
-
-  middleware:
-    outHeaders["x"] = "y"
-  
   onException:
-    # here built-in variables:
-    # `url`, `body` is string
-    # `e` is `ref Exception`
-    echo "URL: ", url
-    echo "BODY: ", body
-    echo "EXCEPTION: [", e.name, "] - ", e.msg
+    echo "Exception"
+    echo e.name
+    echo e.msg
+    statusCode = 422
+    echo statusCode
+    return "Exception"
