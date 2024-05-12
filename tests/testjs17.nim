54218c535c16f0c678f6560d4d6294ef4b7b8cce
Ethosa <social.ethosa@gmail.com>
2024-05-12 11:54:47 +0700

    update testjs17

--- tests/testjs17.nim
+++ tests/testjs17.nim
@@ -15,15 +15,15 @@ proc funcComp1(i: State[int], stmt: TagRef): TagRef =
         i += 1
     stmt
 
-# proc funcComp2(i: State[int]): TagRef =
-#   buildHtml:
-#     tDiv:
-#       "comp2, so `i` is {i}"
+proc funcComp2(i: State[int]): TagRef =
+  buildHtml:
+    tDiv:
+      "comp2, so `i` is {i}"
 
-# proc funcComp3(): TagRef =
-#   buildHtml:
-#     tDiv:
-#       "comp3 without arguments"
+proc funcComp3(): TagRef =
+  buildHtml:
+    tDiv:
+      "comp3 without arguments"
 
 proc funcComp4(stmt: TagRef): TagRef =
   static:
@@ -34,27 +34,26 @@ proc funcComp4(stmt: TagRef): TagRef =
       stmt
 
 
-# component NormalComp:
-#   i: State[int]
-#   html:
-#     tDiv:
-#       "And this is common component. i is {self.i}"
+component NormalComp:
+  i: State[int]
+  html:
+    tDiv:
+      "And this is common component. i is {self.i}"
 
 
 appRoutes "app":
   "/":
     tDiv:
-      # "Here is functional components"
-      # funcComp1(someValue):
-      #   "This is functional component slot"
-      # funcComp2(someValue)
-      # funcComp3()
-      # funcComp3
+      "Here is functional components"
+      funcComp1(someValue):
+        "This is functional component slot"
+      funcComp2(someValue)
+      funcComp3()
+      funcComp3
       funcComp4():
         "Hello"
         funcComp1(someValue):
           "This is functional component slot"
-      !debugCurrent
-      # funcComp4:
-      #   "world"
-      # NormalComp(someValue)
+      funcComp4:
+        "world"
+      NormalComp(someValue)
