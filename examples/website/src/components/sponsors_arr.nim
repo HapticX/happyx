7bab6e262df7b9ed2bae6ece0751d0d6842d2337
Ethosa <social.ethosa@gmail.com>
2024-05-17 23:08:40 +0700

    upgrade website, add new sponsor

--- examples/website/src/components/sponsors_arr.nim
+++ examples/website/src/components/sponsors_arr.nim
@@ -6,17 +6,54 @@ import
 
 proc SponsorsList*(data: seq[Sponsor], sponsorStatus: SponsorStatus = ssDefault,
                    colorBorder: string = DefaultSponsor): TagRef =
+  let color =
+    case sponsorStatus:
+      of ssPlatinum: "0px 5px 51px 0px rgba(179, 212, 238, 0.1)"
+      of ssGold: "0px 5px 51px 0px rgba(250, 204, 21, 0.1)"
+      of ssSilver: "0px 5px 51px 0px rgba(226, 232, 240, 0.1)"
+      of ssDefault: "0px 5px 51px 0px rgba(248, 250, 252, 0.1)"
+  let className =
+    case sponsorStatus:
+      of ssPlatinum: "sponsor-platinum"
+      of ssGold: "sponsor-gold"
+      of ssSilver: "sponsor-silver"
+      of ssDefault: "sponsor-default"
+  let hover =
+    case sponsorStatus:
+      of ssPlatinum: "0px 5px 51px 0px rgba(179, 212, 238, 0.5)"
+      of ssGold: "0px 5px 51px 0px rgba(250, 204, 21, 0.5)"
+      of ssSilver: "0px 5px 51px 0px rgba(226, 232, 240, 0.5)"
+      of ssDefault: "0px 5px 51px 0px rgba(248, 250, 252, 0.5)"
   buildHtml:
     tDiv(class = "w-full grid grid-cols-4 lg:grid-cols-6 xl:grid-cols-10 pt-4 gap-1"):
       for sponsor in data:
         if sponsor.status == sponsorStatus:
-          tImg(
-            src = sponsor.avatarUrl,
-            alt = fmt"{sponsor.name}, ${sponsor.amount}",
-            title = fmt"{sponsor.name}, ${sponsor.amount}",
-            class = "border-[{colorBorder}] border-4 w-24 h-24 rounded-full"
-          ):  
-            @click:
-              {.emit: """//js
-              window.open(`sponsor`.`url`, '_blank').focus();
-              """.}
+          tDiv(
+            class = "bg-black rounded-full {className} transition-all duration-300"
+          ):
+            tA(
+              class = "group relative",
+              href = sponsor.url,
+              target = "_blank"
+            ):
+              tImg(
+                src = sponsor.avatarUrl,
+                alt = fmt"{sponsor.name}, ${sponsor.amount}",
+                class = "border-[{colorBorder}] border-4 w-24 h-24 rounded-full"
+              )
+              tDiv(
+                class = "flex mt-0 group-hover:mt-6 rounded-md text-center text-[{Foreground}] dark:text-[{ForegroundDark}] bg-[{Background}] dark:bg-[{BackgroundDark}] justify-content items-center pointer-events-none absolute delay-150 opacity-0 transition-all duration-300 group-hover:opacity-100"
+              ):
+                {fmt"{sponsor.name}, ${sponsor.amount}"}
+            tStyle: {fmt("""
+              div.<className> {
+                -webkit-box-shadow: <color>;
+                -moz-box-shadow: <color>;
+                box-shadow: <color>;
+              }
+              div.<className>:hover {
+                -webkit-box-shadow: <hover>;
+                -moz-box-shadow: <hover>;
+                box-shadow: <hover>;
+              }
+            """, '<', '>')}
