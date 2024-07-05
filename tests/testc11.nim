9fa5c03123bdf243e0e4ecd942e7e54a61f45694
Ethosa <social.ethosa@gmail.com>
2024-07-06 00:53:44 +0700

    delete test11

--- tests/testc11.nim
+++ tests/testc11.nim
@@ -2,44 +2,44 @@ import ../src/happyx
 import json
 
 
-type MyType = object
-  field: int
+# type MyType = object
+#   field: int
 
-proc `$`*(self: MyType): string =
-  "MyType(a = " & $self.field & ")"
+# proc `$`*(self: MyType): string =
+#   "MyType(a = " & $self.field & ")"
 
-var
-  data = remember @[1, 2, 3]
-  dataArr = remember [1, 2, 3]
-  setData = remember {2, 4, 8, 16, 128}
-  myType = remember MyType(field: 100)
-  jsonVar = remember %*{"a": 1, "b": 2, "c": 3}
-  table = remember newTable[string, string]()
-  tableStr = remember newStringTable()
+# var
+#   data = remember @[1, 2, 3]
+#   dataArr = remember [1, 2, 3]
+#   setData = remember {2, 4, 8, 16, 128}
+#   myType = remember MyType(field: 100)
+#   jsonVar = remember %*{"a": 1, "b": 2, "c": 3}
+#   table = remember newTable[string, string]()
+#   tableStr = remember newStringTable()
 
-echo myType->field
-echo typeof(jsonVar.val)
+# echo myType->field
+# echo typeof(jsonVar.val)
 
-discard data->pop()
-data->add(1)
-data->insert(100, 0)
+# discard data->pop()
+# data->add(1)
+# data->insert(100, 0)
 
-setData->incl(32)
-setData->excl(4)
+# setData->incl(32)
+# setData->excl(4)
 
-echo data
-echo setData
-echo jsonVar
+# echo data
+# echo setData
+# echo jsonVar
 
-data[0] = 0
-dataArr[0] = 0
-table["Hello"] = "World"
-tableStr["Hello"] = "World"
+# data[0] = 0
+# dataArr[0] = 0
+# table["Hello"] = "World"
+# tableStr["Hello"] = "World"
 
-echo data
-echo dataArr
-echo table
-echo tableStr
+# echo data
+# echo dataArr
+# echo table
+# echo tableStr
 
 # for i in data->items():
 #   echo i
