125b3038d9b2be6ef18c9b3a053f2d8f56b0599a
Ethosa <social.ethosa@gmail.com>
2024-09-03 10:48:32 +0700

    fix jvm binds

--- src/happyx/ssr/types.nim
+++ src/happyx/ssr/types.nim
@@ -29,94 +29,184 @@ else:
   import websocketx
   export websocketx
 
-when exportPython or defined(docgen):
-  import
-    nimpy,
-    ../bindings/python_types
-  
-  pyExportModule(name = "server", doc = """
-HappyX web framework [SSR/SSG Part]
-""")
 
-  type
-    Server* = ref object
-      address*: string
-      port*: int
-      routes*: seq[Route]
-      path*: string
-      parent*: Server
-      notFoundCallback*: PyObject
-      middlewareCallback*: PyObject
-      logger*: Logger
-      when enableHttpx:
-        instance*: Settings
-      elif enableHttpBeast:
-        instance*: Settings
-      elif enableMicro:
-        instance*: MicroAsyncHttpServer
-      else:
-        instance*: AsyncHttpServer
-      components*: TableRef[string, BaseComponent]
-    ModelBase* = ref object of PyNimObjectExperimental
-elif exportJvm:
-  import ../bindings/java_types
+when enableDefaultComponents:
+  when exportPython or defined(docgen):
+    import
+      nimpy,
+      ../bindings/python_types
+    
+    pyExportModule(name = "server", doc = """
+  HappyX web framework [SSR/SSG Part]
+  """)
 
-  type
-    Server* = ref object
-      address*: string
-      port*: int
-      logger*: Logger
-      path*: string
-      routes*: seq[Route]
-      parent*: Server
-      title*: string
-      when enableHttpx:
-        instance*: Settings
-      elif enableHttpBeast:
-        instance*: Settings
-      elif enableMicro:
-        instance*: MicroAsyncHttpServer
-      else:
-        instance*: AsyncHttpServer
-      components*: TableRef[string, BaseComponent]
-    ModelBase* = object of RootObj
-elif defined(napibuild):
-  import denim except `%*`
-  import../bindings/node_types
+    type
+      Server* = ref object
+        address*: string
+        port*: int
+        routes*: seq[Route]
+        path*: string
+        parent*: Server
+        notFoundCallback*: PyObject
+        middlewareCallback*: PyObject
+        logger*: Logger
+        when enableHttpx:
+          instance*: Settings
+        elif enableHttpBeast:
+          instance*: Settings
+        elif enableMicro:
+          instance*: MicroAsyncHttpServer
+        else:
+          instance*: AsyncHttpServer
+        components*: TableRef[string, BaseComponent]
+      ModelBase* = ref object of PyNimObjectExperimental
+  elif exportJvm:
+    import ../bindings/java_types
 
-  type
-    Server* = ref object
-      address*: string
-      port*: int
-      logger*: Logger
-      path*: string
-      parent*: Server
-      routes*: seq[Route]
-      title*: string
-      environment*: napi_env
-      when enableHttpx:
-        instance*: Settings
-      elif enableHttpBeast:
-        instance*: Settings
-      elif enableMicro:
-        instance*: MicroAsyncHttpServer
-      else:
-        instance*: AsyncHttpServer
-      components*: TableRef[string, BaseComponent]
-    ModelBase* = object of RootObj
+    type
+      Server* = ref object
+        address*: string
+        port*: int
+        logger*: Logger
+        path*: string
+        routes*: seq[Route]
+        parent*: Server
+        title*: string
+        when enableHttpx:
+          instance*: Settings
+        elif enableHttpBeast:
+          instance*: Settings
+        elif enableMicro:
+          instance*: MicroAsyncHttpServer
+        else:
+          instance*: AsyncHttpServer
+        components*: TableRef[string, BaseComponent]
+      ModelBase* = object of RootObj
+  elif defined(napibuild):
+    import denim except `%*`
+    import../bindings/node_types
+
+    type
+      Server* = ref object
+        address*: string
+        port*: int
+        logger*: Logger
+        path*: string
+        parent*: Server
+        routes*: seq[Route]
+        title*: string
+        environment*: napi_env
+        when enableHttpx:
+          instance*: Settings
+        elif enableHttpBeast:
+          instance*: Settings
+        elif enableMicro:
+          instance*: MicroAsyncHttpServer
+        else:
+          instance*: AsyncHttpServer
+        components*: TableRef[string, BaseComponent]
+      ModelBase* = object of RootObj
+  else:
+    type
+      Server* = object
+        address*: string
+        port*: int
+        logger*: Logger
+        when enableHttpx:
+          instance*: Settings
+        elif enableHttpBeast:
+          instance*: Settings
+        elif enableMicro:
+          instance*: MicroAsyncHttpServer
+        else:
+          instance*: AsyncHttpServer
+        components*: TableRef[string, BaseComponent]
+      ModelBase* = object of RootObj
 else:
-  type
-    Server* = object
-      address*: string
-      port*: int
-      logger*: Logger
-      when enableHttpx:
-        instance*: Settings
-      elif enableHttpBeast:
-        instance*: Settings
-      elif enableMicro:
-        instance*: MicroAsyncHttpServer
-      else:
-        instance*: AsyncHttpServer
-      components*: TableRef[string, BaseComponent]
-    ModelBase* = object of RootObj
+  when exportPython or defined(docgen):
+    import
+      nimpy,
+      ../bindings/python_types
+    
+    pyExportModule(name = "server", doc = """
+  HappyX web framework [SSR/SSG Part]
+  """)
+
+    type
+      Server* = ref object
+        address*: string
+        port*: int
+        routes*: seq[Route]
+        path*: string
+        parent*: Server
+        notFoundCallback*: PyObject
+        middlewareCallback*: PyObject
+        logger*: Logger
+        when enableHttpx:
+          instance*: Settings
+        elif enableHttpBeast:
+          instance*: Settings
+        elif enableMicro:
+          instance*: MicroAsyncHttpServer
+        else:
+          instance*: AsyncHttpServer
+      ModelBase* = ref object of PyNimObjectExperimental
+  elif exportJvm:
+    import ../bindings/java_types
+
+    type
+      Server* = ref object
+        address*: string
+        port*: int
+        logger*: Logger
+        path*: string
+        routes*: seq[Route]
+        parent*: Server
+        title*: string
+        when enableHttpx:
+          instance*: Settings
+        elif enableHttpBeast:
+          instance*: Settings
+        elif enableMicro:
+          instance*: MicroAsyncHttpServer
+        else:
+          instance*: AsyncHttpServer
+      ModelBase* = object of RootObj
+  elif defined(napibuild):
+    import denim except `%*`
+    import../bindings/node_types
+
+    type
+      Server* = ref object
+        address*: string
+        port*: int
+        logger*: Logger
+        path*: string
+        parent*: Server
+        routes*: seq[Route]
+        title*: string
+        environment*: napi_env
+        when enableHttpx:
+          instance*: Settings
+        elif enableHttpBeast:
+          instance*: Settings
+        elif enableMicro:
+          instance*: MicroAsyncHttpServer
+        else:
+          instance*: AsyncHttpServer
+      ModelBase* = object of RootObj
+  else:
+    type
+      Server* = object
+        address*: string
+        port*: int
+        logger*: Logger
+        when enableHttpx:
+          instance*: Settings
+        elif enableHttpBeast:
+          instance*: Settings
+        elif enableMicro:
+          instance*: MicroAsyncHttpServer
+        else:
+          instance*: AsyncHttpServer
+      ModelBase* = object of RootObj
