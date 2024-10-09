268d77075662cdf8da4e795d8e10dbcd7d24f17e
Ethosa <social.ethosa@gmail.com>
2024-10-09 19:43:03 +0700

    improve builtin server perfomance

--- tests/testc3.nim
+++ tests/testc3.nim
@@ -17,5 +17,10 @@ serve("127.0.0.1", 5000):
   post "/user":
     """"""
   
+  get "/some":
+    # echo req.path.get()
+    # echo req.pathWithQueries.get()
+    urlPath
+  
   notfound:
     """method not allowed"""
